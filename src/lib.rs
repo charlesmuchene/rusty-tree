36ab060d2d8e87bfd4f10905cbdcd27a48701508
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2023-04-04 12:23:39 -0500

    Implement listing all files

--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,43 @@
-use std::{fmt};
-use std::fmt::{Formatter};
-use std::fs::{DirEntry};
-use std::io;
-use std::path::{PathBuf};
-use crate::RustyError::{EntryInitializationError, IOError};
+use std::{
+    fmt::{self, Formatter},
+    fs::{ReadDir},
+    io,
+    path::PathBuf,
+};
+
+use crate::RustyError::IOError;
 
 const ROOT_PATH: &str = ".";
-const UNKNOWN_FILE_NAME: &str = "Unknown";
 
 pub fn run() -> Result {
-    let path = PathBuf::from(ROOT_PATH);
-    let entry = path.read_dir()?;
-    for e in entry {
-        let dir = Directory::new(e?)?;
-        dir.traverse()
-    }
+    let root_path = PathBuf::from(ROOT_PATH);
+    let read_dir = root_path.read_dir()?;
+    list(read_dir)?;
     Ok(())
 }
 
-trait Entry {
-    fn name(&self) -> String;
-    fn traverse(&self) {
-        // no-op
-    }
-
-    fn get_name(entry: &DirEntry) -> String {
-        match entry.file_name().into_string() {
-            Ok(name) => name,
-            Err(e) => {
-                eprintln!("Error getting name for {:?}:{:?}", entry, e);
-                UNKNOWN_FILE_NAME.to_string()
-            }
+fn list(read_dir: ReadDir) -> Result {
+    for entry in read_dir {
+        let entry = entry?;
+        let file_type = entry.file_type()?;
+        println!("* {:?}", entry.file_name());
+        if file_type.is_dir() {
+            list(entry.path().read_dir()?)?;
         }
     }
-}
-
-struct Directory {
-    entry: DirEntry,
-}
-
-impl Directory {
-    fn new(entry: DirEntry) -> Result<impl Entry> {
-        // let file_type = entry.file_type().expect("We have no idea");
-        println!("{:?}", entry.file_name());
-        // if file_type.is_dir() {
-        //     Ok(Directory { entry })
-        // } else {
-        //     Err(EntryInitializationError)
-        // }
-        Ok(Directory { entry })
-    }
-}
-
-impl Entry for Directory {
-    fn name(&self) -> String {
-        Directory::get_name(&self.entry)
-    }
-
-    fn traverse(&self) {
-        for entry in self.entry.path().read_dir().expect("Check traverse") {
-            // block(Entry::new(entry?))?
-            println!("Traversing children {:?}", entry)
-        }
-    }
-}
-
-struct File {
-    entry: DirEntry,
-}
-
-impl Entry for File {
-    fn name(&self) -> String {
-        File::get_name(&self.entry)
-    }
+    Ok(())
 }
 
 type Result<T = ()> = std::result::Result<T, RustyError>;
 
 pub enum RustyError {
     IOError(io::Error),
-    EntryInitializationError,
 }
 
 impl fmt::Display for RustyError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             IOError(e) => f.write_fmt(format_args!("{e}")),
-            EntryInitializationError => f.write_str("Couldn't initialize an entry")
         }
     }
 }
