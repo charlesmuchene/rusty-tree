d63692547b58c3824c57176ee7d5c3172fb56140
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2023-04-05 07:15:58 -0500

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,23 @@ use std::{
 
 use crate::RustyError::IOError;
 
+/// Part of a filename prefix to denote an item in the listing.
+///
+/// See `FILENAME_LAST_SYMBOL` for an exception.
 const FILENAME_SYMBOL: &str = "├── ";
+/// Part of a filename prefix to denote the last item in the listing.
 const FILENAME_LAST_SYMBOL: &str = "└── ";
+/// Part of a filename prefix to denote a separator in the listing.
+///
+/// See `FILENAME_LAST_SEPARATOR` for an exception.
 const FILENAME_SEPARATOR: &str = "│  ";
+/// Part of a filename prefix to denote the last item separator in the listing.
 const FILENAME_LAST_SEPARATOR: &str = "  ";
 const CURRENT_DIR: &str = ".";
 
 /// Entry point to listing logic
 ///
-/// Returns a `Result` which is an alias for `std::result::Result<(), RustyError`
+/// Returns a `Result` which is an alias for `std::result::Result<(), RustyError>`
 pub fn run() -> Result {
     let current_path = env::current_dir()?;
     let read_dir = current_path.read_dir()?;
@@ -26,6 +34,8 @@ pub fn run() -> Result {
 }
 
 /// List contents of `ReadDir` using the given `prefix`
+///
+/// [`fs::ReadDir`]: https://doc.rust-lang.org/stable/std/fs/struct.ReadDir.html
 fn list(read_dir: ReadDir, prefix: String) -> Result {
     let mut peekable = read_dir
         .filter(|entry| !is_hidden(entry))
@@ -49,6 +59,8 @@ fn list(read_dir: ReadDir, prefix: String) -> Result {
 }
 
 /// Extract file name from `DirEntry`: defaults to "Unknown"
+///
+/// [`fs::DirEntry`]: https://doc.rust-lang.org/stable/std/fs/struct.DirEntry.html
 fn get_filename(entry: &DirEntry) -> String {
     entry.file_name().to_str().unwrap_or("Unknown").to_string()
 }
@@ -62,7 +74,9 @@ fn filename_symbol<T: Iterator>(peekable: &mut Peekable<T>) -> FilenameSymbol {
     }
 }
 
-/// Determines if the entry pointed to by `entry` is hidden
+/// Determines if the `entry` is hidden
+///
+/// [`fs::DirEntry`]: https://doc.rust-lang.org/stable/std/fs/struct.DirEntry.html
 fn is_hidden(entry: &std::result::Result<DirEntry, io::Error>) -> bool {
     if let Ok(entry) = entry {
         entry.file_name()
@@ -85,8 +99,9 @@ struct FilenameSymbol {
     separator: &'static str,
 }
 
-/// Custom error
+/// Custom error for the program
 pub enum RustyError {
+    /// Wrapper around an `io::Error`
     IOError(io::Error),
 }
 
